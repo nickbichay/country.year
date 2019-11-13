a1f92cc31f9148c3b8636f331cb683614630580f
Nick Bichay <bichayni@msu.edu>
2019-11-13 11:46:01 -0500

    version 0.2.0

---
+++ R/country_year.R
@@ -0,0 +1,52 @@
+
+country.year <- function(data, id, year, dup.omit = TRUE, ranges=TRUE) {
+
+if(dup.omit==FALSE & ranges==TRUE){
+    message("Error: dup.omit is FALSE while ranges is TRUE")
+} else {
+
+    require(R.utils)
+
+    countries <- unique(id)
+
+    mat <- data.frame(row.names=countries)
+    mat$Country <- rownames(mat)
+
+
+if(dup.omit==FALSE) {
+for(i in countries){
+
+    years <- unlist(year)[id==i]
+    years <- sort(years)
+
+    mat$Years[mat$Country==i] <- paste(years, collapse=" ")   
+
+    }
+
+
+} else if(dup.omit==TRUE) {
+for(i in countries){
+
+    years <- unlist(year)[id==i]
+    years <- unique(sort(years))
+
+    if(ranges==FALSE){
+    mat$Years[mat$Country==i] <- paste(years, collapse=" ")   
+
+    } else if(ranges==TRUE){
+    mat$Years[mat$Country==i] <- seqToHumanReadable(years, tau=1) 
+
+    } else { }
+
+
+    }
+
+
+} else { }
+
+
+output  <- mat[order(mat$Country),]
+
+}
+
+}
